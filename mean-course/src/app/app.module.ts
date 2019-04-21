056c8824eb168d4a9e61b8765897f4d986f3ab3a
mpk75234 <mikekennedy75234@gmail.com>
2019-04-21 06:23:52 -0500

    added EventEmitter and AngularMaterialCSS

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material';
+import { MatExpansionModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component'
+import { HeaderComponent } from './header/header-component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core'
+import { Component, EventEmitter, Output} from '@angular/core'
 @Component({
   selector: 'app-post-create',
   templateUrl: 'post-create.component.html',
+  styleUrls: ['./post-create-component.css']
 })
 
 export class PostCreateComponent {
+  enteredTitle = '';
   enteredValue = '';
-  newPost = 'No Content';
+  @Output() postCreated = new EventEmitter();
   onAddPost(){
-    this.newPost = this.enteredValue;
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredValue
+    };
+    this.postCreated.emit(post);
   }
 }
