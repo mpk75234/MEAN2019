f67dd064ce32a915d8d9ba96b75626c47d51b0fa
mpk75234 <mikekennedy75234@gmail.com>
2019-05-05 09:46:46 -0500

    added dummy JSON objects to /api/posts

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -3,8 +3,35 @@ const express = require('express');
 const app = express();
 
 
-app.use((req, res, next)=>{
-  res.send('Hello from Express at port 3000');
+
+app.use('/api/posts',(req, res, next)=>{
+  //res.send('Hello from Express at port 3000');
+  const posts = [
+    {
+      id: 0,
+      title: 'EOS',
+      content: 'gambling on this one'
+    },
+    {
+      id: 1,
+      title: 'ETH',
+      content: 'a must-have for any trader'
+    },
+    {
+      id: 2,
+      title: 'BTC',
+      content: 'a staple of any crypto diet'
+    },
+    {
+      id: 3,
+      title: 'NXS',
+      content: 'taking a risk, hoping it triples'
+    }
+  ];
+  res.status(200).json({
+    message: posts.length + " posts fetched",
+    posts: posts
+  });
 });
 
 module.exports = app;
