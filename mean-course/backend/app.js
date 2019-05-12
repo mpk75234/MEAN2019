436d64f3b3434d2e02eb3a96f3c956e3b6ce3477
mpk75234 <mikekennedy75234@gmail.com>
2019-05-12 10:20:36 -0500

    CR of CRUD now working with MongoDB

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const app = express();
+mongoose.connect('mongodb://mike:crom@localhost:27017/mehsages')
+.then(()=>{
+    console.log('successfully connected to mehsages database')
+    err => {
+        console.log(err.message);
+    }
+});
+const Post = require('./models/post');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -11,9 +21,15 @@ app.use((req, res, next) =>{
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, OPTIONS');
   next();
 });
+//database mehsages collection: mehsages
 //test /api/posts POST functionality
 app.post('/api/posts', (req, res, next) =>{
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  //save it to mongodb :)
+  post.save();
   console.log(post);
   res.status(201).json({
     message: 'post added successfully'
@@ -22,32 +38,13 @@ app.post('/api/posts', (req, res, next) =>{
 
 app.get('/api/posts',(req, res, next)=>{
   //res.send('Hello from Express at port 3000'); kkkk
-  const posts = [
-    {
-      id: "0",
-      title: 'EOS',
-      content: 'gambling on this one'
-    },
-    {
-      id: "1",
-      title: 'ETH',
-      content: 'a must-have for any trader'
-    },
-    {
-      id: "2",
-      title: 'BTC',
-      content: 'a staple of any crypto diet'
-    },
-    {
-      id: "3",
-      title: 'NXS',
-      content: 'taking a risk, hoping it triples'
-    }
-  ];
-  res.status(200).json({
-    message: posts.length + " posts fetched",
-    posts: posts
-  });
+  Post.find({}, {__v: 0})
+  .then((posts =>{
+    res.status(200).json({
+      message: "mehsages are ready",
+      posts: posts
+    });
+  }));
 });
 
 module.exports = app;
